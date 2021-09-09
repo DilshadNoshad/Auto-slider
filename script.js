6de9a9bb0af6bb14c8c6d2684196edf112fe7b1b
DilshadSheikh <dilshadnoshad2003@gmail.com>
2021-09-09 23:55:09 +0500

    initial commit

---
+++ script.js
@@ -0,0 +1,34 @@
+// slider animation
+$('.slide-nav').on('click', function (e) {
+    e.preventDefault();
+    // get current slide
+    var current = $('.flex--active').data('slide'),
+        // get button data-slide
+        next = $(this).data('slide');
+
+    $('.slide-nav').removeClass('active');
+    $(this).addClass('active');
+
+    if (current === next) {
+        return false;
+    } else {
+        $('.slider__warpper').find('.flex__container[data-slide=' + next + ']').addClass('flex--preStart');
+        $('.flex--active').addClass('animate--end');
+        setTimeout(function () {
+            $('.flex--preStart').removeClass('animate--start flex--preStart').addClass('flex--active');
+            $('.animate--end').addClass('animate--start').removeClass('animate--end flex--active');
+        }, 800);
+    }
+});
+
+// slider automation
+
+var count = 1;
+
+setInterval(function(){
+    document.getElementById('click' + count).click();
+    count++;
+    if(count > 4){
+        count = 1;
+    }
+}, 5000);
